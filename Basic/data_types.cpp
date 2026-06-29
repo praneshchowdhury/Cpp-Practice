feaf199a9b748fea16a028736d218f6b1d1e21a9
praneshchowdhury <pranesh.ac.bd@gmail.com>
2026-06-29 21:47:13 +0600

    Add cpp basic programs

---
+++ Basic/data_types.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 10;
+    float y= 10.6;
+    double z= 10.34234;
+    char ch = 'P';
+    bool istrue = true;
+
+    cout << "x = "<< x << endl<< "Y = "<<y <<endl;
+    cout << "Char = " << ch << endl<< "Is true? "<< istrue<<endl<<"Double"<<z;
+
+
+}
